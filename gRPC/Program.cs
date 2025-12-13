5d0dc03a4947bdf87cb74ebbad31314523d80c4c
AlhafiSurface <alhafi@hotmail.com>
2025-12-13 15:55:15 +0300

    add reflection

--- gRPC/Program.cs
+++ gRPC/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection(); // Enable gRPC Reflection
+
 builder.Services.AddDbContext<gRPC.Data.AppDbContext>(op =>
     op.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -13,6 +15,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<PostService>();
+
+// Enable gRPC Reflection in development (for Postman, grpcurl, etc.)
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
